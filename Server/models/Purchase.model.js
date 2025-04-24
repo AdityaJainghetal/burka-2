0a88696c7ffe185ea57685bfde614cf7ad90b292
Aditya Jain <adityajainghetal@gmail.com>
2025-04-24 12:22:07 +0530

    Task

--- Server/models/Purchase.model.js
+++ Server/models/Purchase.model.js
@@ -21,11 +21,22 @@
 const mongoose = require('mongoose');
 
 const purchaseSchema = new mongoose.Schema({
-  product: { 
-    type: mongoose.Schema.Types.ObjectId, 
-    ref: 'Product',
-    required: true 
-  },
+  // product: { 
+  //   type: mongoose.Schema.Types.ObjectId, 
+  //   ref: 'Product',
+  //   required: true 
+  // },
+
+   products: [{
+          product: {
+              type: mongoose.Schema.Types.ObjectId,
+              ref: "product"
+          },
+          quantity: {
+              type: String
+          },
+         
+      }],
   quantity: { type: Number, required: true, min: 1 },
   purchaseDate: { type: Date, default: Date.now },
  
