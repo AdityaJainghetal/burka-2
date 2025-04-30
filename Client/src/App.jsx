c7f7859a36d1b47ac2f6b251b2cfb892e7a91802
Aditya Jain <adityajainghetal@gmail.com>
2025-04-30 11:58:50 +0530

    Task

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -871,7 +871,7 @@ function AppContent({ onLogout }) {
           )}
         </header>
 
-        <main className="flex-1 py-6 px-4 sm:px-6 lg:px-8">
+        <main className="flex-1 max-w-8xl ms-auto py-6 px-4 sm:px-6 lg:px-8">
           <div className="px-4 py-6 sm:px-0">
             <Routes>
               <Route path="/login" element={<Login />} />
