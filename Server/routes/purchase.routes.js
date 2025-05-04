c0ad5346218be43f569c9dac4ed15e4f8ebaa93f
Abhay Dixit <Dixitshivam534@gmail.com>
2025-05-04 22:58:42 +0530

    changes again

--- Server/routes/purchase.routes.js
+++ Server/routes/purchase.routes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { getProductByBarcode, scanAndIncreaseQuantity } = require('../controllers/purchase.controller');
+const { getProductByBarcode, scanAndIncreaseQuantity } = require('../controllers/puchase.controller');
 const router = express.Router();
 
 router.get("/barcode/:barcode", getProductByBarcode);
